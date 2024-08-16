b7f8667adb8239bdeacf2d69b37518e6f477400f
leonid-06 <harinlenya1488@gmail.com>
2024-08-16 19:52:47 +0300

    first commit

---
+++ src/main/java/com/leonid/springcoreunderstanding/SpringCoreUnderstandingApplication.java
@@ -0,0 +1,13 @@
+package com.leonid.springcoreunderstanding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreUnderstandingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreUnderstandingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonid/springcoreunderstanding/SpringCoreUnderstandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonid.springcoreunderstanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreUnderstandingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
