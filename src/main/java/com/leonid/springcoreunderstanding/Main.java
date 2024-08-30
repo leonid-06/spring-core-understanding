7cd318e5f22b314859544182fd8e1257d7aeabe7
leonid-06 <harinlenya1488@gmail.com>
2024-08-30 22:37:38 +0300

    bean via @Component and @ComponentScan

--- src/main/java/com/leonid/springcoreunderstanding/Main.java
+++ src/main/java/com/leonid/springcoreunderstanding/Main.java
@@ -2,11 +2,13 @@ package com.leonid.springcoreunderstanding;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext annotationConfigApplicationContext =
-                new AnnotationConfigApplicationContext(MyConfig.class);
-        Person person = annotationConfigApplicationContext.getBean("china", Person.class);
+                new AnnotationConfigApplicationContext("com.leonid.springcoreunderstanding");
+
+        Person person = annotationConfigApplicationContext.getBean(Person.class);
         System.out.println(person);
     }
 }
--- src/main/java/com/leonid/springcoreunderstanding/Person.java
+++ src/main/java/com/leonid/springcoreunderstanding/Person.java
@@ -2,9 +2,13 @@ package com.leonid.springcoreunderstanding;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Component
 public class Person {
     private String name;
     private int age;
